fb9a7615f06dc5d89d921cc2b1f1d97d25a7bb4e
ELMOUSHAKES <153234763+ELMOUSHAKES@users.noreply.github.com>
2026-07-01 06:34:57 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+// =========================
+// PASSWORD
+// =========================
+
+const PASSWORD = "1234"; // غير الباسورد
+
+const loader = document.querySelector(".loader");
+const container = document.querySelector(".container");
+const unlock = document.getElementById("unlock");
+const password = document.getElementById("password");
+const error = document.getElementById("error");
+const music = document.getElementById("music");
+
+function openWebsite(){
+
+    loader.style.opacity = "0";
+
+    setTimeout(()=>{
+
+        loader.style.display = "none";
+
+        container.style.display = "block";
+
+        music.play().catch(()=>{});
+
+    },600);
+
+}
+
+unlock.onclick = ()=>{
+
+   
